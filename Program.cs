852135afdc36747758c34e7d8e9ae7787d82102f
David Hoare <dhoare12@users.noreply.github.com>
2025-04-04 01:03:28 +0100

    v0.1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace ExcelAssembler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var templatePath = "C:\\Temp\\ExcelAssembler\\TestRepeatTemplate.xlsx";
+            var xmlPath = "C:\\Temp\\ExcelAssembler\\RepeatTest.xml";
+
+            var tmpPath = $"C:\\Temp\\ExcelAssembler\\Output{DateTime.UtcNow:yyyyMMddHHmmss}.xlsx";
+
+            var assembler = new ExcelAssembler();
+            assembler.ProcessTemplate(templatePath, xmlPath, tmpPath);
+
+            var psi = new ProcessStartInfo(tmpPath)
+            {
+                UseShellExecute = true,
+            };
+
+            Process.Start(psi);
+
+        }
+    }
+}
